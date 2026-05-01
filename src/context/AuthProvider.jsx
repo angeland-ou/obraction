0f510309bfc9b3c9972cf92449bdaa3f67f690d8
angeland-ou <angeland@outlook.com>
2026-05-02 01:15:18 +0200

    feat: add handle force logout & checkAuth fix in AuthProvider

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,46 +1,58 @@
-import { useState, useEffect } from 'react';
-import { AuthContext } from './AuthContext'; 
+import { useState, useEffect, useCallback } from 'react';
+import { AuthContext } from './AuthContext';
+import { getMe, logout as logoutService } from '../api/auth';
 
 export const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
 
-    const checkAuth = async () => {
+    const checkAuth = useCallback(async () => {
         try {
-            const res = await fetch('/api/auth/me');
-            const data = await res.json();
-            if (res.ok) setUser(data.user);
+            const userData = await getMe();
+            setUser(userData.data?.user);
         } catch (error) {
-            console.error("Error comprobando autenticación", error)
+            console.error("Error comprobando autenticación", error);
             setUser(null);
         } finally {
             setLoading(false);
         }
-    };
+    }, []);
+
 
     const logout = async () => {
         try {
-            // borramos cookie
-            const res = await fetch('/api/auth/logout', { 
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json' }
-            });
-
-            if (res.ok) {
-                console.log("Sesión cerrada en servidor");
-            }
+            await logoutService();
         } catch (error) {
             console.error("Error al cerrar sesión:", error);
         } finally {
-            setUser(null);
+            setUser(null); // limpiamos siempre el usuario
         }
     };
 
-    useEffect(() => { checkAuth(); }, []);
+    // escuchamos eventos desde http.js
+    useEffect(() => {
+        const handleForceLogout = () => {
+            console.warn("Auth: Force logout (401)");
+            setUser(null); // limpiamos el usuario para redirigir
+        };
+
+        // listener del canal de eventos
+        window.addEventListener('force-logout', handleForceLogout);
+
+        // remove listener
+        return () => {
+            window.removeEventListener('force-logout', handleForceLogout);
+        };
+    }, []);
+
+    useEffect(() => {
+        checkAuth();
+    }, [checkAuth]);
 
     return (
         <AuthContext.Provider value={{ user, loading, logout, setUser, checkAuth }}>
             {children}
         </AuthContext.Provider>
     );
+    
 };
