3c190a77785c990dd450a31ffc81718037fe35ed
angeland-ou <angeland@outlook.com>
2026-05-02 01:16:37 +0200

    fix: use http.js in auth.js

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,30 +1,7 @@
-const BASE_URL = 'http://localhost:3000/api/auth';
+import { postRequest, getRequest } from './http'; // tu archivo con la lógica centralizada
 
-// peticiones post
-const postRequest = async (endpoint, data) => {
-    const response = await fetch(`${BASE_URL}${endpoint}`, {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        // guardamos en navegador la cookie 'accessToken'
-        credentials: 'include', 
-        body: JSON.stringify(data),
-    });
-
-    const result = await response.json();
-    if (!response.ok) throw new Error(result.error || 'Error en la petición');
-    return result;
-};
-
-export const login = (email, password) => postRequest('/login', { email, password });
-
-export const register = (userData) => postRequest('/register', userData);
-
-export const logout = () => postRequest('/logout', {});
-
-//obtenemos datos del usuario (/me)
-export const getMe = async () => {
-    const response = await fetch(`${BASE_URL}/me`, { credentials: 'include' });
-    const result = await response.json();
-    if (!response.ok) throw new Error(result.error);
-    return result.data;
-};
+export const login = (email, password) => postRequest('/auth/login', { email, password });
+export const register = (userData) => postRequest('/auth/register', userData);
+export const logout = () => postRequest('/auth/logout', {});
+export const getMe = () => getRequest('/auth/me');
+export const activateAccount = (token) => getRequest(`/auth/activate/${token}`);
