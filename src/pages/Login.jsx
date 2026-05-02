4c658a1ebd4c7563d44766fd10083b906ad0729a
angeland-ou <angeland@outlook.com>
2026-05-02 02:29:53 +0200

    feat: add validation errors into fields in LogioPage

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -7,10 +7,11 @@ import Button from '../components/Button';
 
 const LoginPage = () => {
     // guardamos al usuario
-    const { setUser } = useAuth(); 
-    
+    const { setUser } = useAuth();
+
     const [formData, setFormData] = useState({ email: '', password: '' });
     const [error, setError] = useState(null);
+    const [fieldErrors, setFieldErrors] = useState({});
     const [isSubmitting, setIsSubmitting] = useState(false);
     const navigate = useNavigate();
 
@@ -21,20 +22,34 @@ const LoginPage = () => {
     const handleSubmit = async (e) => {
         e.preventDefault();
         setError(null);
+        setFieldErrors({});
         setIsSubmitting(true);
 
         try {
             // llamada a endpoint login de backend
             const response = await login(formData.email, formData.password);
-            
+
             // guardamos user en context global -> json -> {user: data.user...}
-            setUser(response.user); 
+            setUser(response.data?.user);
 
             // redirigimos al dashboard
-            navigate('/dashboard'); 
+            navigate('/dashboard');
         } catch (err) {
-            // error con mensaje del backend
-            setError(err.response?.data?.error || "Error al iniciar sesión");
+
+            // console.log("Status:", err.status);
+            // console.log("Data:", err.data?.error);
+            // console.log("Details:", err.data);
+
+            if (err.status === 400 && err.data?.error?.details) {
+                const errors = {};
+                err.data.error.details.forEach(({ path, message }) => {
+                    errors[path[0]] = message;
+                });
+                setFieldErrors(errors);
+            } else {
+                setError(err.data?.error?.message || err.message || "Error al iniciar sesión")
+            }
+
         } finally {
             setIsSubmitting(false);
         }
@@ -43,40 +58,40 @@ const LoginPage = () => {
     return (
         <div className="login-page">
             <form onSubmit={handleSubmit}>
-                <h1>Bienvenido</h1>
+                <h1>Accede a tu cuenta</h1>
                 {error && <div style={{ color: 'red', marginBottom: '10px' }}>{error}</div>}
-                
-                <Input 
+
+                <Input
                     name="email"
                     label="email"
                     icon="mail"
                     type="email"
                     placeholder="Correo electrónico"
                     onChange={handleChange}
-                    
+                    error={fieldErrors.email}
                 />
-                <Input 
+                <Input
                     name="password"
                     label="password"
                     icon="lock"
                     type="password"
                     placeholder="Contraseña"
                     onChange={handleChange}
-                    
+                    error={fieldErrors.password}
                 />
 
                 <Button
-                size = "md"
-                className="right"
-                type="submit"
-                layout="icon-text-button"
-                icon={isSubmitting ? "sync" : "login"} 
-                label={isSubmitting ? "Cargando..." : "Entrar"} 
-                disabled={isSubmitting}
+                    size="md"
+                    className="right"
+                    type="submit"
+                    layout="icon-text-button"
+                    icon={isSubmitting ? "sync" : "login"}
+                    label={isSubmitting ? "Cargando..." : "Entrar"}
+                    disabled={isSubmitting}
                 />
             </form>
-            
-            <p>¿No tienes cuenta? <span className="link" onClick={() => navigate('/register')}>Regístrate</span></p>
+
+            <p style={{ paddingTop: '16px', color: 'var(--text-muted)' }}>¿No tienes cuenta? <span className="link" onClick={() => navigate('/register')}>Regístrate</span></p>
         </div>
     );
 };
