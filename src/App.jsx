a3e6a4000f9b84f9701303e135e9e60cdaba863a
angeland-ou <angeland@outlook.com>
2026-05-02 02:26:22 +0200

    feat: add Theme & Tenant Providers to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import Header from './components/Header';
 import Footer from './components/Footer';
 import { Outlet } from "react-router-dom";
+import { ThemeProvider } from './context/ThemeContext';
+import { TenantProvider } from './context/TenantContext';
 
 const App = () => {
-
-  return (
-    <>
-      <Header />
-      <main><Outlet /></main>
-      <Footer />
-    </>
-  )
-}
+    return (
+        <ThemeProvider>
+            <TenantProvider>
+                <Header />
+                <main><Outlet /></main>
+                <Footer />
+            </TenantProvider>
+        </ThemeProvider>
+    );
+};
 
 export default App;
